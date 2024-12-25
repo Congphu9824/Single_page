78671df39141ba70cca6f1682c6fec5cd4af30ee
Congphu9824 <phamphu872004@gmail.com>
2024-12-25 17:27:35 +0700

    Add project files.

---
+++ Blazor/Program.cs
@@ -0,0 +1,15 @@
+using Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+//Register Syncfusion license
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf0x0RHxbf1x1ZFxMYVlbQXVPIiBoS35RckRhW31ccXVQQ2lVV0Fz");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor();
+
+await builder.Build().RunAsync();
